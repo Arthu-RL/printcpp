7c22cbc4e042191511352e4cc9ceaf80aa8e6430
Arthu-RL <arthurrlima29@gmail.com>
2024-06-09 23:32:14 -0300

    some improvements

--- Print.h
+++ Print.h
@@ -1,31 +1,32 @@
 #ifndef PRINT_H
 #define PRINT_H
 #include <iostream>
+#include "MyString/MyString.h"
 
 struct ConsoleColors
 {
-    const char *reset = "\033[0m";
-    const char *bold = "\033[1m";
+    const MyString reset = "\033[0m";
+    const MyString bold = "\033[1m";
 
     // Normal Colors
-    const char *black = "\033[30m";
-    const char *red = "\033[31m";
-    const char *green = "\033[32m";
-    const char *yellow = "\033[33m";
-    const char *blue = "\033[34m";
-    const char *magenta = "\033[35m";
-    const char *cyan = "\033[36m";
-    const char *white = "\033[37m";
+    const MyString black = "\033[30m";
+    const MyString red = "\033[31m";
+    const MyString green = "\033[32m";
+    const MyString yellow = "\033[33m";
+    const MyString blue = "\033[34m";
+    const MyString magenta = "\033[35m";
+    const MyString cyan = "\033[36m";
+    const MyString white = "\033[37m";
 
     // Light Colors
-    const char *lightBlack = "\033[90m";
-    const char *lightRed = "\033[91m";
-    const char *lightGreen = "\033[92m";
-    const char *lightYellow = "\033[93m";
-    const char *lightBlue = "\033[94m";
-    const char *lightMagenta = "\033[95m";
-    const char *lightCyan = "\033[96m";
-    const char *lightWhite = "\033[97m";
+    const MyString lightBlack = "\033[90m";
+    const MyString lightRed = "\033[91m";
+    const MyString lightGreen = "\033[92m";
+    const MyString lightYellow = "\033[93m";
+    const MyString lightBlue = "\033[94m";
+    const MyString lightMagenta = "\033[95m";
+    const MyString lightCyan = "\033[96m";
+    const MyString lightWhite = "\033[97m";
 } consoleColors;
 
 class Print
@@ -33,6 +34,14 @@ class Print
 public:
     void print_in_color(const char* message, const char* color) {
         std::cout << color << message << consoleColors.reset << std::endl;
+    }
+
+    template<typename... Args>
+    void info(Args... args) {
+        std::cout << "[ ";
+        print_in_color("DEBUG", consoleColors.bold+consoleColors.white);
+        std::cout << " ] ";
+        print(args...);
     }
 
     template<typename... Args>
